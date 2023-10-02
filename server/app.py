f9fea90638d2a35227b417a5f45b778f5c023f78
Sky Bauerschmidt Sweeney <dbauerschmidtsweeney@gmail.com>
2023-10-02 13:44:40 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -22,6 +22,33 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {
+            'message': f"Earthquake {id} not found."
+        }
+        status = 404
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def minimum_magnitude(magnitude):
+    earthquakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        earthquakes.append(quake.to_dict())
+    body = {
+        'count': len(earthquakes),
+        'quakes': earthquakes
+    }
+
+    return make_response(body, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
